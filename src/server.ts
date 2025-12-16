9ea8e771df872006c47f01d86b527258aace1844
ishoef <ismailhossennayeb@gmail.com>
2025-12-16 22:04:22 +0200

    add database initialization and user table creation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import express, { Request, Response } from "express";
 import config from "./config";
+import initDB from "./config/db";
 
 const app = express();
 const port = config.port;
 
+app.use(express.json());
+
+// DB
+initDB();
+
+// Test Route
 app.get("/", (req: Request, res: Response) => {
   res.send("an-nahdah server is live");
 });
 
+// API Routes
+
+
+// Server Listener
 app.listen(port, () => {
   console.log(`An-Nahdah Server is running on the port ${port}`);
 });
